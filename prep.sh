8b01d8e44a579ba787f68b5c2324fb2e862de499
Steven Noonan <steven@uplinklabs.net>
2008-05-08 10:47:00 -0700

    Fixing prep script.

--- prep.sh
+++ prep.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo Configuring Steven\'s git globals...
+if [ $(uname -o) == "Cygwin" ]; then
+	echo Disabling file mode checking...
+	git repo-config core.filemode false
+fi
 git-config --global core.compression 9
 git-config --global color.diff auto
 git-config --global color.status auto
