67bb8c42d2ad300c17a7a90f3e278c7ea61eaa0a
Steven Noonan <steven@uplinklabs.net>
2008-05-08 09:55:17 -0700

    Making configuring git a bit easier.

--- prep.sh
+++ prep.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo Doing checkout of CrissCross...
-svn co https://crisscross.googlecode.com/svn/trunk contrib/CrissCross
+echo Configuring Steven's git globals...
+git-config --global core.compression 9
+git-config --global color.diff auto
+git-config --global color.status auto
+git-config --global color.branch auto
