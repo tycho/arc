afeca6a55d14dafa777e9df74f4548ebdcc81847
Steven Noonan <steven@uplinklabs.net>
2008-10-19 20:25:49 -0700

    update.sh: added for easy updating
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo Updating primary repository...
+git pull origin master
+
+echo Updating submodules...
+MODULES="contrib/crisscross"
+ORIGINAL=`pwd`
+
+for a in $MODULES; do
+	cd $a
+	echo -n "$a: "
+	git pull origin master
+	cd $ORIGINAL
+done
