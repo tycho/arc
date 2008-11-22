731de97a871addd60b2979a813c7ad13ff458ee1
Steven Noonan <steven@uplinklabs.net>
2008-11-22 15:26:55 -0800

    prep-repo.sh: updated for new contrib repo
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- scripts/prep-repo.sh
+++ scripts/prep-repo.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-git submodule init contrib/crisscross
-git submodule update contrib/crisscross
-cd contrib/crisscross
-git checkout master
+git submodule init contrib
+git submodule update contrib
+cd contrib
+git branch -D master
+git checkout -t -b master origin/master
+
+./prep-repo.sh
